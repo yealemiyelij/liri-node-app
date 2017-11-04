63006df2a7ccd56de23c7bfa58a6e499a717a057
yealemiyelij <30580757+yealemiyelij@users.noreply.github.com>
2017-11-04 15:03:51 -0400

    testignore

---
+++ keys.js
@@ -0,0 +1,14 @@
+var keys = {
+    spotifyKeys: {
+        id: "3615f349e58a47e9ad05eb7e6173b5cb",
+        secret: "7baa48fd78244beab431c4a04f278cc3"
+    },
+    twitterKeys: {
+        consumer_key: "iEyYslTCs0rTyhF5iCkQfdWle",
+        consumer_secret: "l3uv7IffaWoq3On8zZ5K1YurwMKl3UnRJRyA92nHOaOzi65aV1",
+        access_token_key: "926537994294702080-Vx6ZVZWs2FYMCRv32So824sUD84Olg6",
+        access_token_secret: "qkm9vrCiEu07asF50wU9rdF58Avo28qd8b9MGLmPW2wTR",
+    }
+}
+
+module.exports = keys;
